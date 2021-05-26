31351ff5b79ef835ece2204132a2d4946496435d
bornedj <bornedj@miamioh.edu>
2021-05-26 18:31:56 -0400

    beginning work on the recommender file. Able to narrow down to the genre depth in the tree.

--- final_portfolio_project/recommender.py
+++ final_portfolio_project/recommender.py
@@ -12,13 +12,28 @@ with open('artist_node_dict.pkl', 'rb') as f:
     artist_nodes = pickle.load(f)
 f.close()
 
+with open('choice_dict_nodes.pkl', 'rb') as f:
+    choice_nodes = pickle.load(f)
+f.close()
 
 # the main function will the wrapper function that displays the in line information and should be callable incase of repeats
 def main():
-    pass
+    print("""Welcome to the music recommender. We will narrow down a recommendation by genre.""")
+    narrow = None
+    while not narrow:
+        narrow = input("To look at list of all genres type \"all\". If you would like to look at the genres under a specific letter type the letter: ").lower()
+
+        if narrow == 'all':
+            for choice in choice_nodes:
+                print(choice, choice_nodes[choice].info)
+        elif narrow in choice_nodes.keys():
+            print(choice_nodes[narrow].info)
+        else:
+            print('Oops, wrong input, try again')
+            narrow = None
 
 
         
 
-
+main()
 
