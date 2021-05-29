437e4a37f647051c7f811b5bde142aaf1235d8a5
bornedj <bornedj@miamioh.edu>
2021-05-29 17:22:51 -0400

    set up the prompt module.

---
+++ fullstack/message_gen/main.js
@@ -0,0 +1,6 @@
+//setting up readline so that we can get user input
+const prompt = require('prompt-sync')();
+
+//getting user name
+const userName = prompt('What is your name? ');
+console.log(userName);
