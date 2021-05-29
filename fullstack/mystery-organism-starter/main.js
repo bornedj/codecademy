d3c47942d465508a46ed2753f5af958182128aa9
bornedj <bornedj@miamioh.edu>
2021-05-28 22:52:06 -0400

    finished first version of compareDNA and mutate, will test

--- fullstack/mystery-organism-starter/main.js
+++ fullstack/mystery-organism-starter/main.js
@@ -1,3 +1,6 @@
+const { throws } = require("node:assert");
+const { count } = require("node:console");
+
 // Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
@@ -15,10 +18,31 @@ const mockUpStrand = () => {
 
 
 //we will need to create several of these dna strands: making factory
-const pAequorFactory = (number, arr){//number is the number of the dna, arr is an array of 15 dna bases(1 strand)
+const pAequorFactory = (number, arr) => {//number is the number of the dna, arr is an array of 15 dna bases(1 strand)
   return {//return obj
     specimenNum: number,
-    dna: arr
+    dna: arr,
+    mutate() {//mutate function changes a selected dna base and changes it to another one
+      const strandIdx = Math.floor(Math.random()*15);// gets a random number between 0 - 14 inclusive
+      const currentStrand = this.dna[strandIdx]//assings it the current strand value
+      let pos;
+      do{//using do while to generate a new value for current strand
+        pos = returnRandBase();//pos for possible value
+      }while (pos === currentStrand)
+
+      //change the strand
+      this.dna[strandIdx] = pos;
+    },// end mutate
+    compareDNA(pAequor) {//checks two strands of dna, if value == at == index increment counter, return formatted string
+      let counter = 0;
+      for (let i = 0; i < this.dna.length; i++){//loop through dna length
+        if (this.dna[i] === pAequor.dna[i]){
+          counter++;
+        }
+      }//end for
+      const percentage = Math.floor((counter / this.dna.length) * 100);
+      console.log(`Specimen ${this.specimenNum} and specimen ${pAequor.specimenNum} have ${percentage}% in common.`)
+    }
   }
 
 }
