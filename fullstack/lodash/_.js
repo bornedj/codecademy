3f80e48400b8177cfe11ba0c50dba3274dcc6e6d
bornedj <bornedj@miamioh.edu>
2021-05-28 20:55:28 -0400

    added the pad and has methods and tested both

--- fullstack/lodash/_.js
+++ fullstack/lodash/_.js
@@ -25,14 +25,36 @@ const _ = {
     //words will split a string into words
     words(string){
         return string.split(' ');
-    }
+    },
 
-    //
+    //padding a string with spaces
+    pad(string, len){
+        if (string.length >= len){
+            return string;
+        }
+        
+        front = Math.floor((len - string.length) / 2);
+        end = len - string.length - front;
+
+        string = ' '.repeat(front) + string + ' '.repeat(end);
+
+        return string;
+    },
+
+    //defining the has method
+    has(obj, key){
+        if (obj[key]){
+            return true;
+        }else{
+            return false;
+        }
+    }
 };
 // // testing clamp
 // console.log(_.clamp(5,10,15))
 // console.log(_.clamp(17,10,15))
 
-
+//testing the pad function, trying recursion
+// console.log(_.pad('hello', 4))
 // Do not write or modify code below this line.
 module.exports = _;
