421a4b93130120557913269f2e93694a4aa1633d
bornedj <bornedj@miamioh.edu>
2021-05-29 17:30:26 -0400

    also retrieved user birthday and created date object

--- fullstack/message_gen/main.js
+++ fullstack/message_gen/main.js
@@ -3,4 +3,11 @@ const prompt = require('prompt-sync')();
 
 //getting user name
 const userName = prompt('What is your name? ');
-console.log(userName);
+
+// get user birthday
+const birthday = prompt('What is your birthday? Please enter it in the MM-DD format: ');
+const birthdate = new Date(birthday);
+
+
+
+
