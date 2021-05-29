9cf323de64ff81b1c65c75d68283491d895368de
bornedj <bornedj@miamioh.edu>
2021-05-29 17:34:51 -0400

    branch will be used to work on the object literals that a sign will be

---
+++ fullstack/message_gen/signs.js
@@ -0,0 +1,8 @@
+// this file will create the object literals that I will use to create the random messages.
+
+//using a factory function to create an object for each star sign
+const signFactory = (name, startDate, endDate, messages) => {
+    return {
+
+    }
+};
