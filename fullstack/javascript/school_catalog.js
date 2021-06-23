3bfac4fc64b24ae4421acd58496e75125f8856a0
bornedj <bornedj@miamioh.edu>
2021-06-23 17:50:27 -0400

    finished the school catalog project

--- fullstack/javascript/school_catalog.js
+++ fullstack/javascript/school_catalog.js
@@ -26,7 +26,7 @@ class School {
     }
 
     quickFacts() {
-        console.log(`${this._name} educates ${this._numberOfStudents} at the ${this._level} level.`);
+        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} level.`);
     }
 
     static pickSubstituteTeacher(substituteTeacher) {
@@ -34,3 +34,34 @@ class School {
         return substituteTeacher[index];
     }
 }
+
+class Primary extends School {
+    constructor(name, numberOfStudents, pickupPolicy){
+        super(name, 'primary', numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+
+}
+
+class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams){
+        super(name, 'High School', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+
+    get sportsTeams() {
+        return this._sportsTeams;
+    }
+}
+
+//testing the primary school
+const lorraineHansbury = new Primary('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']));
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+console.log(alSmith.sportsTeams);
