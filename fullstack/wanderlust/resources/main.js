15656b337e0e568c0d1a8544e3465bc2d8c05078
bornedj <bornedj@miamioh.edu>
2021-06-26 20:01:45 -0400

    added a fetch to foursquare

--- fullstack/wanderlust/resources/main.js
+++ fullstack/wanderlust/resources/main.js
@@ -1,11 +1,11 @@
 // Foursquare API Info
-const clientId = '';
-const clientSecret = '';
-const url = '';
+const clientId = 'ZEPB15XV31OU0R2MFBJAJZ0JLG5ZPNH5LV42Q3ICC0TNUWLA';
+const clientSecret = 'UW4DNUOOJVWNHAYFHR3012HJ2TMPM3UOXX5J4RFZ11DS1UGY';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';//api call with query key near
 
 // OpenWeather Info
-const openWeatherKey = '';
-const weatherUrl = '';
+const openWeatherKey = 'fd746abef0f5255024c4842a6012b17f';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 // Page Elements
 const $input = $('#city');
@@ -17,7 +17,19 @@ const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
-const getVenues = () => {
+const getVenues = async () => {
+    const city = $input.val();
+    const urlToFetch = url + city +"&limit=10%client_id=" + clientId + "&client_secret=" + 
+    clientSecret + "&v=20210626";
+
+    try {
+        const response = await fetch(urlToFetch);
+        if ((response).ok) {
+            console.log(response);
+        }
+    } catch(error) {
+        console.log(error);
+    }
 
 }
 
